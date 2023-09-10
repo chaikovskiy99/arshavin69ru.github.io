998fc475739369f5aae997ba0b5a2b599c3f6eb7
vladimir <arshavin69ru@gmail.com>
2023-09-10 16:53:09 +0530

    client component

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,23 +1,26 @@
+"use client";
 import { Metadata } from 'next';
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { prisma } from '@/db';
 import { redirect } from 'next/navigation';
 
-async function createTodo(data: FormData) {
-    console.log("hello")
-    const title = data.get('title')?.valueOf();
+function createTodo(title: string) {
+    // console.log("hello");
+    // const title = data.get('title')?.valueOf();
     if (typeof title !== 'string' || title.length === 0) {
         throw new Error("Invalid title!");
     }
-
-    await prisma.todo.create({
-        data: {
-            title: title,
-            complete: false
-        }
-    });
-
+    const saveData = async () => {
+        await prisma.todo.create({
+            data: {
+                title: title,
+                complete: false
+            }
+        });
+    };
+    saveData()
+    
     redirect('/');
 }
 
@@ -29,18 +32,19 @@ const meta: Metadata = {
 };
 
 const CreateTodo = () => {
+    const [title, setTitle] = useState("");
     return (
         <>
             <header className='flex justify-between items-center mb-4'>
                 <h1 className='text-2xl'>Create New Todo</h1>
             </header>
-            <form action={createTodo}>
-                <input type="text" name='title' className='border block w-full border-slate-300 bg-transparent rounded px-2 py-1 focus-within:border-slate-100 outline-none' />
+            <form >
+                <input type="text" name='title' value={title} className='border block w-full border-slate-300 bg-transparent rounded px-2 py-1 focus-within:border-slate-100 outline-none' onChange={e => setTitle(e.target.value)} />
             </form>
 
             <div className='flex justify-end p-3'>
                 <Link className='border border-slate-300 bg-transparent rounded px-2 py-1 focus-within:border-slate-100 outline-none' href="..">{"Cancel"}</Link>
-                <button className="border border-slate-300 bg-transparent rounded px-2 py-1 focus-within:border-slate-100 outline-none" type='submit'>Create</button>
+                <button onClick={() => createTodo(title)} className="border border-slate-300 bg-transparent rounded px-2 py-1 focus-within:border-slate-100 outline-none" type='submit'>Create</button>
             </div>
 
         </>
